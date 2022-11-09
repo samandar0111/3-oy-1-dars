bd52ca3ca9625114a8803bea9c9fa8b902407efa
samandar0111 <samandar011@gmail.com>
2022-11-10 00:07:22 +0500

    java

---
+++ main.js
@@ -0,0 +1,32 @@
+
+
+let name = prompt("ismingiz")
+
+let firstname = prompt("familiyangiz")
+
+let age = prompt("yoshingiz")
+
+let manzil = prompt("manzil")
+
+let tel = prompt("tell")
+
+
+
+console.log("ismingiz; " + name)
+console.log("familiyangiz; " + firstname)
+console.log("yoshingiz; " + age)
+
+console.log("manzil; " + manzil)
+
+console.log("tell" + tel)
+
+document.write("ism-")
+document.write( name + `<br>`)
+document.write("familiya-")
+document.write(firstname + `<br>`)
+document.write("yosh-")
+document.write(age + `<br>`)
+document.write("manzil-")
+document.write(manzil + `<br>`)
+document.write("tel-")
+document.write(tel + `<br>`)
